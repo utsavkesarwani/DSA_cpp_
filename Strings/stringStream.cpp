4bf2949f3ab0af9c35099e9b5cca3a9057ec738c
utsavkesarwani <kesarwaniutsav697@gmail.com>
2026-08-17 08:24:03 +0530

    print in a new line

---
+++ Strings/stringStream.cpp
@@ -0,0 +1,19 @@
+// given a sentence , split every single word of the 
+// sentence and print in a new line 
+
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<sstream>
+using namespace std;
+int main(){
+    string str;
+    getline(cin,str);
+    stringstream ss(str);
+    string temp;
+
+    while(ss>>temp){
+        cout<<temp<<endl;
+    }
+}
