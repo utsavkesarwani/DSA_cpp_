2c92b2edd9ae35f832e9b41f7056f52db822953c
utsavkesarwani <kesarwaniutsav697@gmail.com>
2026-08-17 09:09:43 +0530

    stoi_stoll

---
+++ Strings/string2/stoi_stoll.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    // string str = "1234567891";
+    // int x = stoi(str);
+    // cout<<x;
+
+    string str = "1234567891234567891";
+    long long x = stoll(str);
+    cout<<x;
+
+
+}
